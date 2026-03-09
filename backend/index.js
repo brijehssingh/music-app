c0110f3356c98ac43e08f9d983635cbd35520d57
brijehssingh <brijeshkumar4705r@gmail.com>
2026-03-09 23:09:03 +0530

    add root route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import db from "./src/db/dbconn.js";
 
 dotenv.config();
+app.get("/", (req, res) => {
+  res.send("Music App Backend Running 🚀");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   db();
