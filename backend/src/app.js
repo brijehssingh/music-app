0714c05898914eb1b02d096640b97fab751b9079
brijehssingh <brijeshkumar4705r@gmail.com>
2026-03-10 13:12:25 +0530

    fix cors for vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import route from "./auth/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"; 
 const app = express();
 
-
+  app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://music-app-orpin-iota.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
